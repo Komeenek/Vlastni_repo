c30570cb481ad522553ec02b14cafd92eb8636af
komeenek <galaxy.speedrunning@gmail.com>
2025-03-26 14:28:01 +0100

    vyhra

---
+++ top usp/top usp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("top usp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("top usp")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("55521b5f-2ea0-4fcc-87a0-6cf2e0d5adbd")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ vyhra_check/vyhra_check/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vyhra_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[6, 7]{
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 1, 2, 1, 0 },
+            { 2, 2, 2, 2, 2, 1, 0 }
+};
+            int[] position = { 5, 0 };
+            Console.WriteLine(VyhraCheck(board, 5, position));
+
+            Console.ReadLine();
+        }
+
+        static bool VyhraCheck(int[,] matice, int VyherniPocet, int[] pozice)
+        {
+            int posledni_symbol = matice[pozice[0], pozice[1]];
+            
+            bool CheckRadku()
+            {
+                int[] radek = new int[2 * VyherniPocet - 1];
+
+                for (int x = 0; x < 2 * VyherniPocet - 1; x++)
+                {
+                    try
+                    {
+                        radek[x] = matice[pozice[1] - VyherniPocet];
+                    }
+                }
+
+                for (int i = 0; i < VyherniPocet; i++)
+                {
+                    if (radek[i] == posledni_symbol)
+                    {
+                        for (int j = 0; j < VyherniPocet; j++)
+                        {
+                            if (j != posledni_symbol)
+                                return false;
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            return CheckRadku();
+        }
+    }
+}
