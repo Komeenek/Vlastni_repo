9188c8a0b808b1de4f65cc4fbda2a53e72083001
komeenek <galaxy.speedrunning@gmail.com>
2025-04-30 15:51:24 +0200

    vyr strom

---
+++ stromecek_s_vyrazy/stromecek_s_vyrazy/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stromecek_s_vyrazy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup = Console.ReadLine();
+
+            string[] realVstup = vstup.Split(' ');
+
+            Vyhodnocovani itk = new Vyhodnocovani();
+
+            VyrazovyStrom<string> vyrazovyStrom = new VyrazovyStrom<string>();
+
+            vyrazovyStrom.Vytvor(realVstup);
+
+            Console.WriteLine(vyrazovyStrom.Show());
+
+            Console.WriteLine(itk.VyhodnoceniPost(realVstup));
+
+            Console.ReadLine();
+        }
+    }
+    class Vyhodnocovani
+    {
+        public float VyhodnoceniPost(string[] vstup)
+        {
+            Stack<float> cisla = new Stack<float>();
+
+
+            for (int i = 0; i < vstup.Length; i++)
+            {
+                try
+                {
+                    if (float.TryParse(vstup[i], NumberStyles.Float, CultureInfo.InvariantCulture, out float result))
+                        cisla.Push(result);
+                    else
+                    {
+                        switch (Convert.ToChar(vstup[i]))
+                        {
+                            case '+':
+                                Single x = cisla.Pop();
+                                Single y = cisla.Pop();
+                                cisla.Push(y + x);
+                                break;
+                            case '-':
+                                x = cisla.Pop();
+                                y = cisla.Pop();
+                                cisla.Push(y - x);
+                                break;
+                            case '*':
+                                x = cisla.Pop();
+                                y = cisla.Pop();
+                                cisla.Push(y * x);
+                                break;
+                            case '/':
+                                x = cisla.Pop();
+                                y = cisla.Pop();
+                                cisla.Push(y / x);
+                                break;
+                            default:
+                                Console.WriteLine("Něco se pos*alo");
+                                break;
+                        }
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Špatný vstup");
+                }
+            }
+
+            return cisla.Pop();
+        }
+
+    }
+
+    class Node<T>
+    {
+
+        public string Value { get; set; }
+
+        public Node<T> LeftSon { get; set; }
+
+        public Node<T> RightSon { get; set; }
+
+    }
+
+    class VyrazovyStrom<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public void Vytvor(string[] vstup)
+        {
+            Stack<Node<T>> ZasobnikNodu = new Stack<Node<T>>();
+
+            for (int i = 0; i < vstup.Length; i++)
+            {
+                string prvek = vstup[i];
+
+                Node<T> uzlik = new Node<T>();
+                uzlik.Value = prvek;
+                try
+                {
+                    if (!float.TryParse(prvek, NumberStyles.Float, CultureInfo.InvariantCulture, out float result))
+                    {
+                        uzlik.RightSon = ZasobnikNodu.Pop();
+                        uzlik.LeftSon = ZasobnikNodu.Pop();
+                    }
+                    ZasobnikNodu.Push(uzlik);
+                }
+
+                catch
+                {
+                    Console.WriteLine("spatne!");
+                }
+            }
+
+            Root = ZasobnikNodu.Pop();
+        }
+
+        public string Show()
+        {
+
+            string Stringovnik = "";
+
+            void _show(Node<T> node)
+            {
+                if (node == null)
+                    return;
+
+                Stringovnik += "(";
+
+                _show(node.LeftSon);
+
+                Stringovnik += node.Value + " ";
+
+                _show(node.RightSon);
+
+                Stringovnik += ")";
+            }
+
+            if (Root == null)
+                return "Strom je prazdny";
+            _show(Root);
+
+            return Stringovnik;
+        }
+
+    }
+}
+
---
+++ stromecek_s_vyrazy/stromecek_s_vyrazy/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("stromecek_s_vyrazy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("stromecek_s_vyrazy")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("e1656011-ef4f-4cad-92f0-f3f3d16f9e6b")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
