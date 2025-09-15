97b8ea98b030365f74ad8015e5c31744d1b2b0a1
Komeenek <galaxy.speedrunning@gmail.com>
2025-09-15 09:36:52 +0200

    MrBeast

---
+++ BiL/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] pole = NacteniVstupu();
+
+            VypsaniPole(pole);
+            for (int i = 0; i < 20; i++)
+            {
+                Krok(pole);
+                VypsaniPole(pole);
+            }
+
+            Console.ReadLine();
+        }
+
+        static char[,] NacteniVstupu()
+        {
+
+            int sirka = Convert.ToInt32(Console.ReadLine());
+            int vyska = Convert.ToInt32(Console.ReadLine());
+
+            char[,] pole = new char[vyska,sirka];
+
+            for (int i = 0; i < vyska; i++) 
+            { 
+                string radek = Console.ReadLine();
+
+                for (int j = 0; j < sirka; j++)
+                {
+                    pole[i, j] = radek[j];
+                }
+            }
+
+            return pole;
+        }
+
+        static void VypsaniPole(char[,] pole)
+        {
+            for (int i = 0;i < pole.GetLength(0);i++)
+            {
+                string stringovnik = "";
+                for (int j = 0;j < pole.GetLength(1);j++)
+                {
+                    stringovnik += pole[i, j];
+                }
+                Console.WriteLine(stringovnik);
+            }
+        }
+
+        static int[] NajdiMrBeasta(char[,] pole)
+        {
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                    if (pole[i, j] == '>')
+                        return new int [3] {i, j, 0};
+                    else if (pole[i, j] == 'v')
+                        return new int[3] { i, j, 1};
+                    else if (pole[i, j] == '<')
+                        return new int[3] { i, j, 2};
+                    else if (pole[i, j] == '^')
+                        return new int[3] { i, j, 3};
+            }
+            return null;
+        }
+
+        static char[,] Krok(char[,] pole)
+        {
+            int[] pozice = NajdiMrBeasta(pole);
+            bool spravne = false;
+            while (!spravne)
+            {
+                if 
+            }
+
+
+            return pole;
+        } 
+    }
+}
